ff521343e872a8b76d01bb416cfc15179b550e2e
devinkapelanski <devinkapelanski@yahoo.com>
2018-09-20 18:31:27 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -20,6 +20,6 @@ function whileLoop(n) {
 function doWhileLoop(array) {
   do {
     array.pop();
-  } while (array.length > 0 && incrementVariable())
+  } while (array.length > 0 && incrementVariable());
 
 }
